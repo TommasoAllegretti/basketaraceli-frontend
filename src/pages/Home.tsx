1181147e5a4ae7ab840c5a671c5ed0d6806c9773
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-07-14 23:48:24 +0200

    added shadcn-ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      <Button>Click me</Button>
+    </>
+  )
 }
