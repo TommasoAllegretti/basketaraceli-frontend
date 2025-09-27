e437cd96ebc6dcaf428030be2355ff413832df2a
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-09-27 23:14:49 +0200

    fixed typo

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -44,7 +44,7 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) 
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle>Login to your account1</CardTitle>
+          <CardTitle>Login to your account</CardTitle>
           <CardDescription>Enter your email below to login to your account</CardDescription>
         </CardHeader>
         <CardContent>
